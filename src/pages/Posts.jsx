4c8d99104366fa4a8ec30299ae986aeefe70ab85
anactasi2607 <anactasi2607@yandex.ru>
2023-08-26 13:54:53 +0300

    Динамическая подгрузка

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useMemo, useRef} from 'react';
 
 import '../styles/App.css';
 
 import { usePosts } from '../hooks/usePosts';
 import { useFetching } from '../hooks/useFetching';
+import { useObserver } from '../hooks/useObserver';
 
 import { getPageCount } from '../utils/pages';
 
@@ -15,29 +16,39 @@ import PostList from '../components/PostList';
 import BaseButton from '../components/UI/button/BaseButton';
 import BaseModal from '../components/UI/modal/BaseModal';
 import BaseLoader from '../components/UI/loader/BaseLoader';
-import BasePagination from '../components/UI/pagination/BasePagination';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
+  const [limit] = useState(10);
   const [page, setPage] = useState(1);
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+  const lastElement = useRef();
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async(limit, page) => {
     const response = await PostService.getAllPosts(limit, page);
 
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count'];
+
     setTotalPages(getPageCount(totalCount, limit))
   })
 
+  const hasPosts = useMemo(() => {
+    return posts.length > 0;
+  }, [posts]);
+
   useEffect(() => {
     fetchPosts(limit, page)
-  }, []);
+  }, [page]);
+
+  useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1);
+  })
 
   function createNewPost(post) {
     setPosts([...posts, {...post, id: Date.now()}]);
@@ -47,11 +58,6 @@ function App() {
   function removePost(post) {
     setPosts(posts.filter(element => element.id !== post.id));
   };
-
-  function changePage(page) {
-    setPage(page);
-    fetchPosts(limit, page);
-  }
 
   return (
     <div className="App">
@@ -64,18 +70,25 @@ function App() {
       <hr className="border"/>
 
       <PostFilter filter={filter} setFilter={setFilter} />
-      {
-        postError
-        ? <h1>Произошла ошибка ${postError}</h1>
-        : isPostsLoading
-          ? <div className="loader"><BaseLoader /></div>
-          : <div>
-            <PostList posts={sortedAndSearchedPosts} remove={removePost} title="Список постов" />
-            {/* TODO: пересмотреть стили везде */}
 
-            <BasePagination page={page} totalPages={totalPages} changePage={changePage} />
-          </div>
+      {
+        postError &&
+        <h1>Произошла ошибка ${postError}</h1>
       }
+
+      {
+        isPostsLoading &&
+        <div className="loader"><BaseLoader /></div>
+      }
+
+      {
+        hasPosts &&
+        <div>
+          <PostList posts={sortedAndSearchedPosts} remove={removePost} title="Список постов" />
+        </div>
+      }
+
+      <div ref={lastElement}>{isPostsLoading && hasPosts && <div className="loader"><BaseLoader /></div>}</div>
     </div>
   );
 }
